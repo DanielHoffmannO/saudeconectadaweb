b98604a7c14d9108534399455cd3e6e498996f41
Daniel Ortiz Hoffmann Bonicio <daniel.ortiz@havan.com.br>
2026-06-25 18:36:44 -0300

    feat: configurar Vercel para servir front + apontar API Railway

--- web/js/config.js
+++ web/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'http://localhost:5000',
+  API_URL: 'https://saudeconectada-production.up.railway.app',
   APP_NAME: 'SaúdeConectada',
   TOKEN_KEY: 'sc_token',
   USER_KEY: 'sc_user'
